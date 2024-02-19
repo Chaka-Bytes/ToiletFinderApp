6c7551d9cd1e926ba8951f3ea2dec8dd5d75273f
mkilic20 <mkilic20@ku.edu.tr>
2024-02-19 23:53:41 +0300

    main page is adjusted

--- toilet_app/lib/main.dart
+++ toilet_app/lib/main.dart
@@ -3,51 +3,33 @@ import 'package:toilet_app/pages/map_page.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() async {
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MapPage(),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
   String _responseText = ''; // Variable to hold the response from the server
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
   }
 
   Future<void> fetchData() async {
     var url = Uri.parse(
-        'http://localhost:8000/toilets'); // Update the URL with your backend endpoint
+        'http://10.0.2.2:8000/toilets'); // Update the URL with your backend endpoint
     var response = await http.get(url);
 
-    print(response.statusCode); // Print the response status code
+    //print the status code of the response in terminal
+    print(response.statusCode);
 
     if (response.statusCode == 200) {
       var data = jsonDecode(response.body);
@@ -61,47 +43,18 @@ class _MyHomePageState extends State<MyHomePage> {
             'Failed to load data'; // Update responseText with an error message
       });
     }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
+    print(_responseText);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Response from Server:', // Display a label for the response
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                _responseText, // Display the response text
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _counter++;
-          });
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const MapPage(),
     );
   }
 }

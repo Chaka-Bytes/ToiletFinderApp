0dcb2856910dc50cb7456ee46351351b9ea0f85f
mkilic20 <mkilic20@ku.edu.tr>
2024-02-22 21:21:09 +0300

    User Location Added, Markers Updated

--- toilet_app/lib/main.dart
+++ toilet_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:toilet_app/pages/map_page.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _responseText = ''; // Variable to hold the response from the server
+  var data = [];
+  LocationData? locationData = null;
+  bool isLoading = true;
 
   @override
   void initState() {
@@ -27,34 +30,39 @@ class _MyAppState extends State<MyApp> {
     var url = Uri.parse(
         'http://10.0.2.2:8000/toilets'); // Update the URL with your backend endpoint
     var response = await http.get(url);
-
-    //print the status code of the response in terminal
-    print(response.statusCode);
-
     if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
       setState(() {
-        _responseText =
-            data.toString(); // Update the responseText with the data received
-      });
-    } else {
-      setState(() {
-        _responseText =
-            'Failed to load data'; // Update responseText with an error message
+        data = jsonDecode(response.body);
+        isLoading = false;
       });
     }
-    print(_responseText);
+    print(data);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MapPage(),
-    );
+    if (isLoading) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Text('Please wait...'),
+          ),
+        ),
+      );
+    } else {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MapPage(data: data),
+      );
+    }
   }
 }
--- toilet_app/lib/pages/map_page.dart
+++ toilet_app/lib/pages/map_page.dart
@@ -1,40 +1,116 @@
 import 'dart:ffi';
-
+import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 
 class MapPage extends StatefulWidget {
-  const MapPage({super.key});
+  final dynamic data; // Define a parameter to receive data
+
+  const MapPage({Key? key, required this.data}) : super(key: key);
 
   @override
   State<MapPage> createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
-  static const LatLng _KocUni = LatLng(41.20621054727754, 29.072417560915472);
+  static const LatLng _Topkapi = LatLng(41.013227881087566, 28.983042421483965);
+  Location _locationController = new Location();
+  LatLng? _currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationUpdates();
+  }
+
+  Set<Marker> _createMarkers() {
+    Set<Marker> markers = {};
+    // Create markers using the received data
+    dynamic data = widget.data;
+    print(data);
+    for (var item in data) {
+      markers.add(
+        Marker(
+          markerId: MarkerId(item['id'].toString()),
+          icon: BitmapDescriptor.defaultMarker,
+          position: LatLng(item['latitude'], item['longitude']),
+          infoWindow: InfoWindow(title: item['name']),
+        ),
+      );
+    }
+
+    // Create marker for user's location if locationData is not null
+    if (_currentP != null) {
+      markers.add(
+        Marker(
+          markerId: MarkerId('user_location'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: _currentP!,
+          infoWindow: InfoWindow(title: 'Your Location'),
+        ),
+      );
+    }
+
+    //if locationData is null return green marker on _Topkapi
+    if (_currentP == null) {
+      markers.add(
+        Marker(
+          markerId: MarkerId('topkapi'),
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          position: _Topkapi,
+          infoWindow: InfoWindow(title: 'default position'),
+        ),
+      );
+    }
+
+    return markers;
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged
+        .listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          _currentP =
+              LatLng(currentLocation.latitude!, currentLocation.longitude!);
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: _KocUni, zoom: 15.0),
-        markers: {
-          Marker(
-            markerId: MarkerId('KocUni'),
-            icon: BitmapDescriptor.defaultMarker,
-            position: _KocUni,
-            infoWindow: InfoWindow(title: 'Koc University'),
-          ),
-          Marker(
-            markerId: MarkerId('KocUniRumeliFeneri'),
-            icon: BitmapDescriptor.defaultMarker,
-            position: LatLng(41.19910700415913, 29.072074238185998),
-            infoWindow:
-                InfoWindow(title: 'Koc University Rumeli Feneri Campus'),
-          ),
-        },
-      ),
+      body: _currentP == null
+          ? const Center(
+              child: Text("Loading..."),
+            )
+          : GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: _currentP!, zoom: 14.0),
+              markers: _createMarkers(),
+            ),
     );
   }
 }
